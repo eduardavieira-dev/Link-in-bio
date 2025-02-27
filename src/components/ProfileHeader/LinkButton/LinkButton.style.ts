ac1d5570540e0f6d3f9bcabe82b814de2d09739f
Eduarda Vieira <eduarda.vieira.goncalves7@gmail.com>
2025-02-26 23:24:25 -0300

    chore: button color

--- src/components/ProfileHeader/LinkButton/LinkButton.style.ts
+++ src/components/ProfileHeader/LinkButton/LinkButton.style.ts
@@ -1,36 +1,38 @@
 import styled from 'styled-components';
 
 export const Button = styled.a`
-    width: 100% ;
+    width: 100%;
     margin-bottom: 2vh;
     border-radius: 5px;
     padding: 14px 44px;
     background-color: var(--white-color);
-    background: linear-gradient(
-    45deg,
-    #9b34ef 0%,
-    #490cb0 20%,
-    #490cb0  60%,
-    transparent 80%
-    );
+    background: linear-gradient(45deg, 
+        #9b34ef 0%, 
+        #6728d3 20%, 
+        #5518bf 40%, 
+        #490cb0 60%, 
+        rgb(82, 4, 161) 80%, 
+        rgb(82, 4, 161, 0) 100%);
     background-size: 250%;
+    background-position: 0%; 
     color: var(--white-color);
     outline: 2px solid transparent;
     text-align: center;
     text-decoration: none;
-    transition: background 1s, outline 1s, transform 1s;
+    transition:
+        background-position .8s ease,
+        outline 0.3s ease,
+        transform 0.3s ease;
 
-    
     &:link,
     &:visited {
-        /* color: var(--bg-color); */
         color: var(--white-color);
     }
 
     &:hover {
-        background: transparent;
+        background-position: 100%;
         outline: 2px solid var(--outline);
         color: var(--white-color);
         transform: scale(1.03);
     }
-    `;
+`;
